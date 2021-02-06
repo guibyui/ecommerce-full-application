1b1e5a643c62b44af97d6a441b8a2ea63f80cd23
Gui Nascimento <55475010+guibyui@users.noreply.github.com>
2021-02-06 13:34:18 -0700

    Week 4 done.

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
+// const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -16,12 +19,44 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findById("601eea4aa501a3612cfc4042")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+  .connect(
+    "mongodb+srv://gui-cse341:gui-cse341@e-commerce.ceecg.mongodb.net/shopDB?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Gui",
+          email: "gui@gui.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = process.env.PORT || 3000;
 
-app.listen(port);
+// app.listen(port);
---
+++ util/database.js
@@ -0,0 +1,29 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://gui-cse341:gui-cse341@e-commerce.ceecg.mongodb.net/shopDB?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No Database Found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
